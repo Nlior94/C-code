2535f493cbff05468fafca9585663cea2a3fcd4f
Nlior <108601166+Nlior94@users.noreply.github.com>
2022-08-05 21:41:38 +0800

    Update game.h

--- game.h
+++ game.h
@@ -1,17 +1,14 @@
 #pragma once
 
-#include <stdlib.h>
-#include <time.h>
+#define ROW 9
+#define COL 9
 
-#define row 3
-#define col 3
+#define ROWS ROW + 2
+#define COLS COL + 2
 
-void clean(char board[row][col], int r, int c);//清空棋盘
+//初始化棋盘
+void clean(char board[ROWS][COLS], int rows, int clos, char set);
 
-void printboard(char board[row][col], int r, int c);//打印棋盘
-
-void playermove(char board[row][col], int r, int c);//玩家下棋
-
-void computermove(char board[row][col], int r, int c);//电脑下棋
-
-char WON(char board[row][col], int r, int c);//判断输赢
+//打印棋盘
+void display(char board[ROWS][COLS], int row, int clo);
+//注意此时传过去的棋盘依然是用的ROWS和COLS
