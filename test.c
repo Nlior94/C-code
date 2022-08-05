8d97318d98125376174e0fbbf7baee1032450b52
Nlior <108601166+Nlior94@users.noreply.github.com>
2022-08-05 21:42:06 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,79 +1,48 @@
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
-#include <string.h>
 #include "game.h"
 
 void menu()
 {
-	printf("***********************\n");
-	printf("******  1. play  ******\n");
-	printf("******  0. exit  ******\n");
-	printf("***********************\n");
+	printf("************************\n");
+	printf("******** 1.play ********\n");
+	printf("******** 0.exit ********\n");
+	printf("************************\n");
 }
 
 void game()
 {
-	char board[row][col];//想改变棋盘格数可以直接在头文件中修改
-	clean(board, row, col);//初始化键盘函数
-	printboard(board,row,col);//打印棋盘
+	char mine[ROWS][COLS] = { 0 };//布置雷
+	char show[ROWS][COLS] = { 0 };//排查雷
+	clean(mine, ROWS, COLS, '0');//初始化为0
+	clean(show, ROWS, COLS, '*');//初始化为*
 
-	char ret = 0;
+	display(mine, ROW, COL);
+	display(show, ROW, COL);
 
-	while (1)
-	{
-		playermove(board, row, col);
-		printboard(board, row, col);
-		ret = WON(board, row, col);//WON函数判断输赢
-		if (ret != 'c')
-			break;
-
-		computermove(board, row, col);
-		printboard(board, row, col);
-		ret = WON(board, row, col);
-		if (ret != 'c')
-			break;
-	}
-	if (ret == '*')
-	{
-		printf("恭喜你，你赢了！\n");
-		printboard(board, row, col);
-	}
-	else if (ret == '#')
-	{
-		printf("很可惜，你输了！\n");
-		printboard(board, row, col);
-	}
-	else
-	{
-		printf("平局\n");
-		printboard(board, row, col);
-	}
 }
 
 int main()
 {
 	int input = 0;
-
-	srand((unsigned int)time(NULL));
-
 	do
 	{
 		menu();
-		printf("请选择>>>");
+		printf("请选择>>>\n");
 		scanf("%d", &input);
-
 		switch (input)
 		{
 		case 1:
+			printf("开始扫雷\n");
 			game();
 			break;
 		case 0:
 			printf("退出游戏\n");
 			break;
 		default:
-			printf("请输入正确指令\n");
+			printf("输入错误值，请重新输入\n");
 			break;
-
 		}
 	} while (input);
 
